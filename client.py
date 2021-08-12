19eefa7bf0f367e1cce31457b2517387f4b59052
yangbc <201870105@smail.nju.edu.cn>
2021-08-13 01:45:29 +0800

    一æ对server的重构，将为一个Node分配一个线程，，重构很多细节，加入很多ä¸异常处理，对client也准备重构，一个任务一个线程，到但是线程之间的通信有待解决ï

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 from socket import *
+from threading import *
 import time
+
 
 
 class Client:
@@ -7,11 +9,16 @@ class Client:
     Client负责做与游戏无关的与服务器的交互
     """
     server_addr = ('127.0.0.1', 10001)
+    threads = {}
     debug = True
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def process_msg(self):
+        while True:
+            info_list = self.recv()
 
     def connect(self):
         self.socket = socket(AF_INET, SOCK_STREAM)
@@ -21,35 +28,54 @@ class Client:
             print("Cannot connect server")
             self.socket.close()
             return False
+        t = Thread(target=self.process_msg)
+        t.start()
         return True
 
     def get_time(self):
         return '['+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+']'
 
-    def send(self, msg):
+    def send(self, msg): # msg 是一个list 如 ["login", "username", "password"]
+        """
+        >>> msg = ["login", "username", "password"]
+        >>> ",".join(msg)
+        'login,username,password'
+        >>> " ".join(msg)
+        'login username password'
+        """
         if self.debug == True:
-            print(self.get_time() + "send to server: " + msg)
+            print(self.get_time() + "send to server: " + " ".join(msg))
+        msg = ",".join(msg) # 用","做分割是因为如果username中有空格不好办
         self.socket.send(msg.encode('utf-8'))
 
     def recv(self):
         recv_msg = self.socket.recv(1024).decode('utf-8').strip()
-        info_list = recv_msg.split(' ')
+        info_list = recv_msg.split(',')
         if self.debug == True:
-            print(self.get_time() + "recieve from server: ", recv_msg)
+            print(self.get_time() + "recieve from server: ", " ".join(info_list))
         return info_list
 
 
     def login(self):
-        if self.connect() == True:
-            msg = "login {} {}".format(self.username, self.password)
+        if self.connect() is True:
+            msg = ["login", self.username, self.password]
             self.send(msg)
             info_list = self.recv()
             if info_list[0] == 'login':
                 if info_list[1] == 'ok':
                     print("login success")
+                    # 分出一个线程监听socket端口
                 else:
                     print(info_list[2])
-        time.sleep(2)
+        # time.sleep(2)
+
+    def register(self, mail_box="2414836228@qq.com", process="step_1"):
+        request = "register"
+        if process == "step_1":
+            msg = [request, process, mail_box]
+            self.send(msg)
+
+
 
 
 
@@ -57,9 +83,9 @@ if __name__ == '__main__':
 
     client = Client(username="yangbc", password="yangbc")
     client.login()
-    client2 = Client(username="zls", password="zls")
-    client2.login()
-    client3 = Client(username='yuanye', password="yuanye")
-    client3.login()
+    # client2 = Client(username="zls", password="zls")
+    # client2.login()
+    # client3 = Client(username='lvyaqiao', password="lvyaqiao")
+    # client3.login()
 
 
--- server.py
+++ server.py
@@ -1,19 +1,13 @@
 from socket import *
-from threading import Thread
+from threading import *
 import sqlite3
-import os, sys
+# import os
+# import sys
 import time
-
-def msg_eval(message):
-    """
-    将从socket中接受到的字符串解析成代码
-    """
-    words = message.split()
-    if words[0] == "login":
-        assert len(words) == 3, "login error"
-        username = words[1]
-        passwrod = words[2]
-
+import random
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
 
 class Server:
@@ -24,28 +18,16 @@ class Server:
     clients = {}
     threads = {}
 
-    def __init__(self, maxClient=3 ,server_addr=('127.0.0.1',10001), database="user.db"):
+    def __init__(self, maxClient=3, server_addr=('127.0.0.1', 10001)):
         self.server_addr = server_addr
         self.maxClient = maxClient
         self.serverSocket = socket()
-        self.serverSocket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+        self.serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.serverSocket.bind(self.server_addr)
+        self.lock = Lock()
 
     def get_time(self):
-        return '['+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+']'
-
-
-    def send_to(self, socket, msg):
-        if self.debug == True:
-            print(self.get_time() + "send to client: " + msg)
-        socket.send(msg.encode('utf-8'))
-
-    def recv_from(self, socket):
-        recv_msg = socket.recv(1024).decode('utf-8').strip()
-        info_list = recv_msg.split(' ')
-        if self.debug == True:
-            print(self.get_time() + "recieve from client: ", recv_msg)
-        return info_list
+        return '[' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ']'
 
     def start(self):
         self.serverSocket.listen(self.maxClient)
@@ -54,70 +36,208 @@ class Server:
 
             conn, addr = self.serverSocket.accept()
 
-            t = Thread(target=self.do_request, args=(conn, ))
+            t = Node(conn, self)
             t.start()
             self.threads[conn] = t
             for c in self.threads:
                 if c._closed is True:
                     self.threads[c].join()
 
-    def do_request(self, conn):
+
+class Node(Thread):
+    """Sever中接受了客户端的一个tcp连接请求，建立tcp连接，对于服务器，将客户端抽象为一个节点，分配一个线程处理这个节点发出的信息
+    """
+    debug = True
+    sender_mailbox = "201870105@smail.nju.edu.cn"
+    sender_nickname = "Game Vslord"
+    sender_password = "Yangbc30"
+    mail_title = "Verify your email for Vslord"
+    nickname = "Player"
+    database = "user.db"
+
+    username = None
+    password = None
+    mail_box = None
+    code = None
+
+    def __init__(self, conn, server):
+        super().__init__()
+        self.socket = conn
+        self.server = server
+
+    def get_time(self):
+        return '[' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ']'
+
+    def send(self, msg):
+        if self.debug is True:
+            print(self.get_time() + "send to client: " + " ".join(msg))
+        msg = ",".join(msg)
+        self.socket.send(msg.encode('utf-8'))
+
+    def recv(self):
+        recv_msg = self.socket.recv(1024).decode('utf-8').strip()
+        info_list = recv_msg.split(',')
+        if self.debug is True:
+            print(self.get_time() + "recieve from client: ", " ".join(info_list))
+        return info_list
+
+    def sql(self, command):
+        """传入符合规范的sql命令字符串记得最后加分号命令全部大写，然后修改/或查询结果
+
+        修改成功 -> 返回True
+        查询成功 -> 返回查询结果 形式为一个列表，每个元素都是元组，每个元组代表一行结果 [('yangbc',), ('yuanye',), ('lvyaqiao',)]
+        失败 -> 返回相应错误的字符串
+        >>> node = Node(socket(), Server())
+
+        >>> node.sql("SELECT sender_password FROM user;")
+        [('yangbc',), ('yuanye',), ('lvyaqiao',), ('zls',)]
+        >>> node.sql("SELECT sender_password FROM user WHERE username = 'yangbc';")
+        [('yangbc',)]
+        >>> node.sql("SELECT sender_password FROM user WHERE username = 'asdf';")
+        []
+        """
+        assert sqlite3.complete_statement(command), "invalid sql command"  # 检查输入的命令是否是合法的sql
+        db = sqlite3.connect(self.database)
+        cursor = db.cursor()
+        command = command.strip()
+        try:
+            cursor.execute(command)
+
+            if command.lstrip().startswith("SELECT"):
+                return cursor.fetchall()  #
+
+        except sqlite3.Error as error:
+            return str(error)
+        finally:
+            cursor.close()
+            db.commit()
+            db.close()
+        return True  # 如果command不以SELECT开头，并且执行成功，返回True
+
+    def send_mail(self, reciever_mailbox="2414836228@qq.com",
+                  reciever_nickname="yuanye"):  # 2414836228@qq.com, 20087000@smail.nju.edu.cn
+        """
+        >>> node = Node(socket(), Server())
+        >>> true_or_false, code_or_error = node.send_mail()
+        >>> true_or_false
+        True
         """
 
+        code = str(random.randrange(1000, 9999))
+        msg = MIMEText("验证码: " + code, 'plain', 'utf-8')
+        msg['From'] = formataddr((self.sender_nickname, self.sender_mailbox))
+        msg['To'] = formataddr((reciever_nickname, reciever_mailbox))
+        msg['Subject'] = self.mail_title
+        try:
+            with smtplib.SMTP_SSL("smtp.exmail.qq.com", 465) as server:  # 因为连接smtp服务器后总要有sever.quit()，直接简写with
+                server.login(self.sender_mailbox, self.sender_password)
+                server.sendmail(self.sender_mailbox, [reciever_mailbox], msg.as_string())
+
+        except Exception as error:
+            return False, str(error)  # 如果smtp报错，将报错发回
+        else:
+            return True, code  # 无异常，表示邮件发送成功，返回验证码，给do_request进行比对
+
+    def run(self):
+        """Node 继承Thread类，继承Thread的类自动调用run方法
         """
+
+        class NetworkError(Exception):  # 为了格式统一，定义一个异常
+            pass
+
         while True:
             try:
-                info_list = self.recv_from(conn)
+                info_list = self.recv()
+                if info_list == ['']:  # 如果说服务器端套间字不断收到['']，说明客户端已经断开连接，这时通信中止，服务器端也应当关闭套间字
+                    raise NetworkError
             except OSError:
-                print("系统已关闭端口")
+                print("服务器端断开连接")
+                return
+            except NetworkError:
+                print("客户端断开连接")
+                self.socket.close()
                 return
 
-            if info_list == ['']:
-                conn.close()
-                print('断开连接')
+            self.process_msg(info_list, len(info_list))
 
-            if info_list[0] == 'login':
-                username = info_list[1]
-                password = info_list[2]
+    def process_msg(self, info_list, info_len):
+        """根据run中不断获取的客户端信息，服务器端进行反馈
 
-                db = sqlite3.connect("user.db")
-                cursor = db.cursor()
-                cursor.execute("select password from user where username = '{}'".format(username) )
-                query_result = cursor.fetchall()   #  [('yangbc',)]
-                cursor.close()
-                db.commit()
-                db.close()
+        """
+        request = info_list[0]
 
-                if query_result == []:
-                    msg = "login error user_not_found"
-                    self.send_to(conn, msg)
-                    conn.close()
+        if request == 'login':
+            username = info_list[1]
+            password = info_list[2]
+
+            query_result = self.sql("SELECT password FROM user WHERE username = '{}';".format(username))
+
+            if query_result == []:
+                msg = [request, "error", "user_not_found"]
+                self.send(msg)
+                self.socket.close()
+            else:
+                password_queried = query_result[0][0]
+                if password == password_queried:
+                    msg = [request, "ok"]
+                    self.send(msg)
+                    # 登录成功
+                    self.username = username  # 将username存入Node类中
+
+                    self.server.lock.acquire()
+                    self.server.clients[username] = Node
+                    self.server.lock.release()
+                    # 将client加入clients字典中，多线程共享
                 else:
-                    password_queried = query_result[0][0]
-                    if  password == password_queried:
-                        msg = "login ok"
-                        self.send_to(conn, msg)
+                    msg = [request, "error", "password_not_correct"]
+                    self.send(msg)
+                    self.socket.close()
+
+        if request == "register":
+            progress = info_list[1]
+
+            if progress == "step_1":
+                reciever_box = info_list[2]  # 根据协议解析box
+                query_result = self.sql("SELECT * FROM user WHERE mailbox = '{}';".format(
+                    reciever_box))  # 如果在数据库中已经存在这个邮箱（self.sql 返回一个非空list），提示客户端邮箱已注册
+                if isinstance(query_result, list):
+                    if len(query_result) != 0:
+                        msg = [request, progress, "error", "mail_box_used"]
+                        self.send(msg)
                     else:
-                        msg = "login error password_not_correct"
-                        self.send_to(conn, msg)
-                        conn.close()
+                        true_or_false, code_or_error = self.send_mail(reciever_box, self.nickname)
+                        if true_or_false is True:
+                            msg = [request, progress, "continue"]
+                            self.send(msg)
+                            self.code = code_or_error  # 将验证码进行记录，下一次客户端发送验证码时进行比对
+                            self.mail_box = reciever_box  # 将邮箱进行记录，如果第二部验证码正确，录入注册信息
 
+                        else:
+                            msg = [request, progress, "error", code_or_error]
+                            self.send(msg)
+                else:
+                    msg = [request, progress, "error", query_result]
+                    self.send(msg)
 
+            elif progress == "step_2":
+                code = info_list[2]
+                if code == self.code:
+                    msg = [request, progress, "continue"]
+                    self.send(msg)
+                else:
+                    msg = [request, progress, "error", "wrong_code"]
+                    self.send(msg)
+            elif progress == "step_3":
+                username = info_list[2]
+                password = info_list[3]
 
-
-class Client:
-    """
-    接受到一个tcp请求创建一个用户，同时创建一个线程
-    不断监听用户的connection处理接收到的内容
-    """
-    def __init__(self, socket, address):
-        self.socket = socket
-        self.address = address
+                self.sql("INSERT INTO user(username, password, mailbox ) VALUES ('{}', '{}', '{}');".format(username,
+                                                                                                            password,
+                                                                                                            self.mail_box))
+                msg = [request, progress, "ok"]
+                self.send(msg)
 
 
 if __name__ == '__main__':
     server = Server()
     server.start()
-
-
-
