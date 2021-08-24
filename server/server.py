9533eb42efed95d9cc09381322c16139784e2521
yangbc <201870105@smail.nju.edu.cn>
2021-08-25 01:06:04 +0800

    有点摸鱼

---
+++ server/server.py
@@ -0,0 +1,248 @@
+from socket import *
+from threading import *
+import sqlite3
+import random
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
+import private
+from utils import *
+from private import *
+
+
+class Server:
+    """
+    服务器，通过端口创建，接受到tcp请求创建client，并且接受client的请求
+    """
+    clients = {}
+    threads = {}
+
+    def __init__(self, maxClient=3, server_addr=('127.0.0.1', 10001)):
+        self.server_addr = server_addr
+        self.maxClient = maxClient
+        self.lock = Lock()  # TODO 多线程修改数据用线程锁
+        try:
+            self.serverSocket = socket()
+            self.serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            self.serverSocket.bind(self.server_addr)
+        except Exception as error:
+            print_by_time(error)
+
+    def start(self):
+        self.serverSocket.listen(self.maxClient)
+        if GLOBAL_DEBUG is True:
+            print_by_time("系统等待连接")
+        while True:
+            conn, addr = self.serverSocket.accept()
+
+            t = Node(conn, self)
+            t.start()
+            self.threads[conn] = t
+            for c in self.threads:
+                if c._closed is True:  # thread._closed 是啥 TODO
+                    self.threads[c].join()
+
+
+class Node(Thread):
+    """Sever中接受了客户端的一个tcp连接请求，建立tcp连接，对于服务器，将客户端抽象为一个节点，分配一个线程处理这个节点发出的信息
+    """
+    sender_mailbox = private.MAILBOX
+    sender_password = private.PASSWORD
+    sender_nickname = "Game Vslord"
+    mail_title = "Verify your email for Vslord"
+    nickname = "Player"
+    database = "user.db"
+    state = {}  # 表示对应用户当前的状态 如{"login":"ok"}
+
+    username = None
+    password = None
+    mailbox = None
+    code = None
+
+    def __init__(self, conn, server):
+        super().__init__()
+        self.socket = conn
+        self.server = server
+
+    def send(self, msg_list):  # send 不需要异常处理，因为如果对方端口关闭，会自动给我发送['']此时recv的异常处理使端口关闭
+        if GLOBAL_DEBUG is True:
+            print_by_time("send to client: " + " ".join(msg_list))
+        msg = ",".join(msg_list)
+        self.socket.send(msg.encode('utf-8'))
+        self.state[msg_list[0]] = msg_list[1]  # 保存状态
+
+    def recv(self):  # 异常交给Node.run处理
+        recv_msg = self.socket.recv(1024).decode('utf-8').strip()
+        info_list = recv_msg.split(',')
+        if GLOBAL_DEBUG is True:
+            print_by_time("recieve from client: " + " ".join(info_list))
+        return info_list
+
+    def sql(self, command):
+        """传入符合规范的sql命令字符串记得最后加分号命令全部大写，然后修改/或查询结果
+
+        修改成功 -> 返回True
+
+        查询成功 -> 返回查询结果 形式为一个列表，每个元素都是元组，每个元组代表一行结果 [('yangbc',), ('yuanye',), ('lvyaqiao',)]
+
+        失败 -> 打印相应错误的字符串
+        >>> node = Node(socket(), Server())
+
+        >>> node.sql("SELECT sender_password FROM user;")
+        [('yangbc',), ('yuanye',), ('lvyaqiao',), ('zls',)]
+        >>> node.sql("SELECT sender_password FROM user WHERE username = 'yangbc';")
+        [('yangbc',)]
+        >>> node.sql("SELECT sender_password FROM user WHERE username = 'asdf';")
+        []
+        """
+        assert sqlite3.complete_statement(command), "invalid sql command"  # 检查输入的命令是否是合法的sql
+        db = sqlite3.connect(self.database)
+        cursor = db.cursor()
+        command = command.strip()
+        try:
+            cursor.execute(command)
+
+            if command.lstrip().startswith("SELECT"):
+                return cursor.fetchall()
+
+        except sqlite3.Error as error:
+            print_by_time(str(error))
+            return False  # TODO 引用方异常处理
+        finally:
+            cursor.close()
+            db.commit()
+            db.close()
+        return True  # 如果command不以SELECT开头，并且执行成功，返回True
+
+    def send_mail(self, reciever_mailbox="2414836228@qq.com",
+                  reciever_nickname="yuanye"):  # 2414836228@qq.com, 20087000@smail.nju.edu.cn
+        """
+        >>> node = Node(socket(), Server())
+        >>> true_or_false, code_or_error = node.send_mail()
+        >>> true_or_false
+        True
+        """
+
+        code = str(random.randrange(1000, 9999))
+        msg = MIMEText("验证码: " + code, 'plain', 'utf-8')
+        msg['From'] = formataddr((self.sender_nickname, self.sender_mailbox))
+        msg['To'] = formataddr((reciever_nickname, reciever_mailbox))
+        msg['Subject'] = self.mail_title
+        try:
+            with smtplib.SMTP_SSL("smtp.exmail.qq.com", 465) as server:  # 因为连接smtp服务器后总要有sever.quit()，直接简写with
+                server.login(self.sender_mailbox, self.sender_password)
+                server.sendmail(self.sender_mailbox, [reciever_mailbox], msg.as_string())
+
+        except Exception as error:
+            print_by_time(error)
+            return False, str(error)  # 如果smtp报错，将报错发回
+        else:
+            return True, code  # 无异常，表示邮件发送成功，返回验证码，给process_msg处理
+
+    def run(self):
+        """Node 继承Thread类，继承Thread的类自动调用run方法
+        """
+
+        while True:
+            try:
+                info_list = self.recv()
+                if info_list == ['']:  # 如果说服务器端套间字不断收到['']，说明客户端已经断开连接，这时通信中止，服务器端也应当关闭套间字
+                    raise NetworkError
+            except OSError as error:
+                print_by_time("服务器端断开连接" + str(error))
+                return
+            except NetworkError as error:
+                print_by_time("客户端断开连接" + str(error))
+                self.socket.close()
+                return
+
+            self.process_msg(info_list, len(info_list))
+
+    def process_msg(self, info_list, info_len):
+        """根据run中不断获取的客户端信息，服务器端进行反馈
+
+        """
+        action = info_list[0]
+
+        if action == "login":
+            username = info_list[1]
+            password = info_list[2]
+
+            query_result = self.sql("SELECT password FROM user WHERE username = '{}';".format(username))
+
+            if query_result == []:
+                msg = [action, "error", "user_not_found"]
+                self.send(msg)
+                self.socket.close()
+            else:
+                password_queried = query_result[0][0]
+                if password == password_queried:
+                    msg = [action, "ok"]
+                    self.send(msg)  # 登录成功
+                    self.username = username  # 将username存入Node类中
+
+                    self.server.lock.acquire()
+                    self.server.clients[username] = Node
+                    self.server.lock.release()
+                    # 将client加入clients字典中，多线程共享
+                else:
+                    msg = [action, "error", "password_not_correct"]
+                    self.send(msg)
+                    self.socket.close()
+
+        if action == "register_step_1":
+
+            username = info_list[1]
+            password = info_list[2]
+            query_result = self.sql("SELECT username FROM user WHERE username = '{}';".format(username))
+            if query_result == []:  # username 唯一，合法，保存数据
+                self.username = username
+                self.password = password
+                msg = [action, "ok"]
+                self.send(msg)
+            else:  # username 不唯一，不合法，发送错误代码
+                msg = [action, "error", "username_used"]
+                self.send(msg)
+
+        if action == "register_step_2":
+            reciever_box = info_list[1]  # 根据协议解析box
+            query_result = self.sql("SELECT * FROM user WHERE mailbox = '{}';".format(
+                reciever_box))  # 如果在数据库中已经存在这个邮箱（self.sql 返回一个非空list），提示客户端邮箱已注册
+            if isinstance(query_result, list):
+                if len(query_result) != 0:
+                    msg = [action, "error", "mailbox_used"]
+                    self.send(msg)
+                else:
+                    true_or_false, code_or_error = self.send_mail(reciever_box, self.nickname)
+                    if true_or_false is True:
+                        msg = [action, "ok"]
+                        self.send(msg)
+                        self.code = code_or_error  # 将验证码进行记录，下一次客户端发送验证码时进行比对
+                        self.mailbox = reciever_box  # 将邮箱进行记录，如果register_step_3验证码比对正确，录入注册信息
+
+                    else:
+                        msg = [action, "error", "cannot_send_mail", code_or_error]  # SMTP 发生错误
+                        self.send(msg)
+            else:
+                msg = [action, "error", "sql_error", query_result]
+                self.send(msg)
+
+        if action == "register_step_3":
+            code = info_list[1]
+            if code == self.code:
+                msg = [action, "ok"]
+                self.state["login"] = "ok"  # 验证码填写正确的同时就登录上了
+                self.send(msg)
+                assert self.sql(
+                    "INSERT INTO user(username, password, mailbox ) VALUES ('{}', '{}', '{}');".format(self.username,
+                                                                                                       self.password,
+                                                                                                       self.mailbox)), "sql insert error"
+            else:
+                msg = [action, "error", "wrong_code"]
+                self.send(msg)
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.start()
---
+++ server/vslord_server.py
@@ -0,0 +1,501 @@
+import builtins
+from vslord_actions import *
+from threading import *
+from tkinter import *
+from tkinter import ttk
+from utils import *
+import ctypes
+
+
+# 测试环境
+class Client:
+    state = {}
+    msg_to_obey = []
+
+
+###########
+# Classes #
+###########
+
+class MyWidget:
+    _master = None
+    _owner = None
+    _displaying = False
+
+    def __init__(self, owner=None, *args, **kwargs):
+        self.attachment = []
+        self.appearance = None
+        self.owner = owner
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):  # 不参与display
+        if isinstance(self.owner, MyWidget):  # 如果前主人继承我的组件类（其中含有subordinate这个lst）把我在前主人的lst中删去
+            self.owner.attachment.remove(self)
+        self._owner = owner  # 修改owner
+        if self.appearance:
+            self.destory()
+        if isinstance(self.owner, MyWidget):  # 重建从属关系，最后重新显示，显示的时候根据从属关系，建一个appr得到atta的master
+            self.owner.attachment.append(self)
+
+    @property
+    def master(self):
+        if type(self) is GameState:
+            self._master = None
+        assert self.owner.appearance, "上级还未创建appr {} {}".format(self.owner, self.owner.appearance)
+        self._master = self.owner.appearance
+        return self._master
+
+    @property
+    def displaying(self):
+        return self._displaying
+
+    @displaying.setter
+    def displaying(self, value):
+        assert value is True or value is False, "_displaying不支持的value"
+        self._displaying = value
+
+    @timing
+    def display(self):
+        if self.appearance is None:  #
+            self.set_appr()
+        for widget in self.attachment:
+            widget.display()
+        self.place_atta()
+        self.displaying = True
+
+    def set_appr(self):  # 附属的外貌在init中已经创建，但是需要创建本身的外貌
+        pass
+
+    def place_atta(self):  # 一个组件可以摆放附属的位置
+        pass
+
+    def forget(self):  # 将一个组件隐藏
+        self.appearance.pack_forget()
+        self.appearance.grid_forget()
+        self.appearance.place_forget()
+
+    def pack(self, *args, **kwargs):
+        assert isinstance(self.appearance, Widget), "self.appearance 不是一个tkinter的widget"
+        self.appearance.pack(*args, **kwargs)
+
+    def grid(self, *args, **kwargs):
+        assert isinstance(self.appearance, Widget), "self.appearance 不是一个tkinter的widget"
+        self.appearance.grid(*args, **kwargs)
+
+    def place(self, *args, **kwargs):
+        assert isinstance(self.appearance, Widget), "self.appearance 不是一个tkinter的widget"
+        self.appearance.place(*args, **kwargs)
+
+    def config(self, *args, **kwargs):
+        assert isinstance(self.appearance, Widget)
+        self.appearance.config(*args, **kwargs)
+
+    def destory(self):
+        self.appearance.destroy()
+        self.appearance = None
+
+
+class Timer(MyWidget):
+
+    def display(self):
+        pass
+
+
+class Card(MyWidget):
+
+    def __init__(self, feature=(None, None), *args,
+                 **kwargs):  # feature 为None显示扑克背面, (1, 0) 前一个代表大小, 后一个代表花色, 花色0: 红心, 1:方块, 2:梅花, 3:黑桃
+        super(Card, self).__init__(*args, **kwargs)
+        self.point, self.color = feature
+        self.selected = False
+        self.to_set_img = True
+
+    @property
+    def value(self):
+        return self.point, self.color
+
+    def select(self, event):
+        if self.selected is False:
+            event.widget.place(y=-20)
+            self.selected = True
+        else:
+            event.widget.place(y=0)
+            self.selected = False
+
+    def set_appr(self):
+        self.appearance = ttk.Label(master=self.master)
+        if self.owner.can_select:
+            self.appearance.bind("<Button-1>", self.select)
+            print(1)
+
+    @property
+    def img(self):
+        if self.owner.anonymous is True:
+            return CARD_IMG["None_None"].subsample(4, 4)
+        else:
+            return CARD_IMG["{}_{}".format(self.point, self.color)].subsample(4, 4)
+
+    def set_img(self):
+        if self.to_set_img is True:
+            img = self.img
+            self.appearance["image"] = img
+            self.appearance.image = img
+            self.to_set_img = False
+        else:
+            pass
+
+    def place_atta(self):
+        pass
+
+    def __eq__(self, other):
+        assert type(other) is Card
+        if self.point == other.point and self.color == other.color:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        assert type(other) is Card
+        order_dict = {None: 0, 3: 1, 4: 2, 5: 3, 6: 4, 7: 5, 8: 6, 9: 7, 10: 8, 11: 9, 12: 10, 13: 11, 1: 12, 2: 13,
+                      14: 14,
+                      15: 15}
+        return order_dict[self.point] < order_dict[other.point]
+
+    def __gt__(self, other):
+        return other < self
+
+    def __str__(self):
+        point_to_str = {None: "未知", 1: "A", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10",
+                        11: "J", 12: "Q", 13: "K", 14: "小猴", 15: "大猴"}
+        color_to_str = {None: "", 0: "红心", 1: "方块", 2: "梅花", 3: "黑桃"}
+        return "Card: {} {} ".format(color_to_str[self.color], point_to_str[self.point])
+
+
+class Cards(MyWidget):
+    def __init__(self, cards=(), can_select=False, anonymous=True, *args, **kwargs):  # cards = ((1, 0), (2, 0))
+        super(Cards, self).__init__(*args, **kwargs)
+        self.can_select = can_select
+        self.anonymous = anonymous
+        print(self.can_select)
+        for card in cards:
+            Card(feature=card, owner=self)
+
+    @property
+    def value(self):
+        lst = []
+        for card in self.card_list:
+            lst.append(card.value)
+        return tuple(lst)
+
+    @property
+    def card_list(self):  # 一个Card对象的list
+        return self.attachment
+
+    @property
+    def cards_selected(self):
+        assert self.can_select
+        lst = []
+        for card in self.card_list:
+            if card.selected is True:
+                lst.append(card.value)
+        return Cards(tuple(lst))
+
+    def set_appr(self):
+        self.appearance = ttk.Frame(master=self.master)
+        # self.appearance = ttk.Frame(master=self.master, bg="grey")
+
+    def place_atta(self):
+        x_border = 0.1
+        y_border = 0.1
+        num = len(self.card_list)
+        if num == 0:
+            step = 0
+            x_border = 0.4
+        else:
+            step = 0.8 / num
+        for index, card in builtins.enumerate(self.card_list):
+            card.set_img()
+            card.place(relx=step * index + x_border, rely=y_border)
+
+    @staticmethod
+    def swap_card(a, b):
+        assert type(a) is Card and type(b) is Card
+        a.point, b.point = b.point, a.point
+        a.color, b.color = b.color, a.color
+        a.selected, b.selected = b.selected, a.selected
+        a.to_set_img, b.to_set_img = True, True
+
+    @timing
+    def sort_cards(self):  # 不参与display
+        num = len(self.card_list)
+        for i in range(num - 1):
+            for j in range(num - 1 - i):
+                if self.card_list[j] > self.card_list[j + 1]:
+                    Cards.swap_card(self.card_list[j], self.card_list[j + 1])
+
+    def __add__(self, other):
+        assert type(other) is Cards, "只能用Cards加Cards"
+        for card in list(other.card_list):
+            card.owner = self
+        if self.anonymous is False:
+            self.sort_cards()
+        if self.displaying is True:
+            self.display()
+
+    def __sub__(self, other):
+        assert type(other) is Cards, "只能用Cards加Cards"
+        for card in list(self.card_list):
+            if card in other.card_list:
+                card.owner = "Trash"
+        if self.displaying is True:
+            self.display()
+
+    def __str__(self):
+        return "Cards: " + str([str(card) for card in self.card_list]) + " "
+
+
+class Info(MyWidget):
+    def __init__(self, name, info, *args, **kwargs):
+        super(Info, self).__init__(*args, **kwargs)
+        self.name = name
+        self.info = info
+
+    def set_appr(self):
+        self.appearance = ttk.Frame(master=self.master)
+        self.Label_01 = ttk.Label(master=self.appearance, text=self.name)
+        self.Label_02 = ttk.Label(master=self.appearance, text=str(self.info))
+        self.Label_01.pack()
+        self.Label_02.pack()
+
+    def place_atta(self):
+        pass
+
+    def __str__(self):
+        str_dict = {"name": self.name}
+        str_dict.update(self.info)
+        return "Info: " + str(str_dict) + " "
+
+
+class Obeyer(MyWidget):
+    def obey(self, info_lst):
+        pass
+
+    @property
+    def cards(self):
+        return Cards()
+
+    def receive_cards(self, cards):
+        assert type(cards) is Card or type(cards) is Cards
+        self.cards.__add__(cards)
+
+    def remove_cards(self, cards):
+        assert type(cards) is Card or type(cards) is Cards
+        self.cards.__sub__(cards)
+
+
+class Desk(Obeyer):
+
+    def __init__(self, *args, **kwargs):
+        super(Desk, self).__init__(*args, **kwargs)
+        Cards(cards=(), owner=self, anonymous=False)
+
+    @property
+    def cards(self):  # 一个Cards对象
+        return self.attachment[0]
+
+    def set_appr(self):
+        self.appearance = ttk.Frame(master=self.master)
+
+    def place_atta(self):
+        self.cards.place(x=0, y=100, relwidth=1, relheight=1)
+
+    def __str__(self):
+        return "Desk: {} ".format(self.cards)
+
+
+class Player(Obeyer):
+    def __init__(self, name, info, can_select=False, anonymous=True, *args, **kwargs):
+        super(Player, self).__init__(*args, **kwargs)
+        self.name = name
+        Info(name=name, info=info, owner=self)
+        Cards(cards=(), can_select=can_select, anonymous=anonymous, owner=self)
+
+    @property
+    def info(self):  # 一个Info对象
+        return self.attachment[0]
+
+    @property
+    def cards(self):  # 一个Cards对象
+        return self.attachment[1]
+
+    def set_appr(self):
+        self.appearance = ttk.Frame(master=self.master)
+
+    def place_atta(self):
+        self.info.pack()
+        self.cards.place(x=0, y=100, relwidth=1, relheight=1)
+
+    def __str__(self):
+        return "Player: " + str(self.info, self.cards)
+
+
+class ActionBar(MyWidget):
+    def __init__(self, *args, **kwargs):
+        super(ActionBar, self).__init__(*args, **kwargs)
+
+    def set_appr(self):
+        self.appearance = ttk.Frame(master=self.master)
+        button_1 = ttk.Button(master=self.appearance, text="不要", command=self.owner.pass_round)
+        button_2 = ttk.Button(master=self.appearance, text="出牌", command=self.owner.drop_cards_request)
+        button_1.pack()
+        button_2.pack()
+
+    def place_atta(self):
+        pass
+
+
+class Me(Player):
+    def __init__(self, client=None, *args, **kwargs):
+        super(Me, self).__init__(can_select=True, anonymous=False, *args, **kwargs)
+        ActionBar(owner=self)
+        self.client = client
+
+    @property
+    def selected_cards(self):
+        return self.cards.cards_selected
+
+    @property
+    def action_bar(self):
+        return self.attachment[2]
+
+    def place_atta(self):
+        super(Me, self).place_atta()
+        self.action_bar.pack()
+
+    def pass_round(self):
+        action_name = "pass_round"
+        print("不要")
+        # action = Action(self.owner, action_name)
+
+    def drop_cards_request(self):
+        print("出牌", self.selected_cards)
+
+
+
+    def drop_cards_implement(self):
+        pass
+
+
+class GameState(MyWidget):
+    """GameState 开启游戏，记录游戏信息，可供gui渲染
+    """
+
+    def __init__(self, me, player_info, current_player, client, *args, **kwargs):
+        """保存gamestate基础信息, 初始化一些组件
+        """
+        super(GameState, self).__init__(*args, **kwargs)
+
+        Me(name=me, info=player_info[me], owner=self, client=client)
+        for name in player_info:
+            if name != me:
+                Player(name=name, info=player_info[name], owner=self)
+        Desk(owner=self)
+        self.me = me
+        self.current_player = current_player
+        self.client = client
+
+    def send(self, msg):
+        pass
+
+        # Card(feature=(1, 1), owner=self)
+
+    @property
+    def player_dict(self):
+        player_dict = {}
+        for player in self.attachment:
+            if isinstance(player, Player):
+                player_dict[player.name] = player
+        return player_dict
+
+    def start_game(self, display=False, master=None):
+        """开始游戏。
+        """
+        thread_obey = Thread(target=self.let_obey)
+        thread_obey.start()
+        if display is True:
+            self.root = master
+            self.start_gui()
+        # thread_obey.join()
+
+    def let_obey(self):
+        """强迫玩家做出某些action
+        """
+        try:
+            while True:
+                for info_list in list(CLIENT.msg_to_obey):
+                    player = self.player_dict[info_list[2]]
+                    player.obey(info_list)
+                    # obey完action之后将该action踢出队列
+                    LOCK.acquire()
+                    CLIENT.msg_to_obey.remove(info_list)
+                    LOCK.release()
+                    # 修改值的时候加锁, save_msg有可能同时修改
+        except Exception:  # TODO 异常处理
+            return
+
+    def set_appr(self):
+        """开启所有对象的display方法"""
+        assert type(self.root) is Tk
+        for w in self.root.winfo_children():
+            w.destroy()
+
+        self.root.title("斗地主")
+        self.root.geometry("1000x800")
+        self.appearance = self.root
+
+    @timing
+    def place_atta(self):
+        # self.attachment[0].config(bg="green")
+        self.attachment[0].place(relx=0.3, rely=0.5, relwidth=0.4, relheight=0.5)
+        # self.attachment[1].config(bg="red")
+        self.attachment[1].place(relx=0, rely=0, relwidth=0.3, relheight=0.5)
+        # self.attachment[2].config(bg="red")
+        self.attachment[2].place(relx=0.7, rely=0, relwidth=0.3, relheight=0.5)
+        # self.attachment[3].config(bg="yellow")
+        self.attachment[3].place(relx=0.3, rely=0, relwidth=0.4, relheight=0.5)
+        print(self.attachment[0].cards.appearance)
+
+        self.attachment[0].receive_cards(Cards(test_cards(CARDS, 18)))
+        self.attachment[1].receive_cards(Cards(test_cards(CARDS, 18)))
+        self.attachment[2].receive_cards(Cards(test_cards(CARDS, 18)))
+        self.attachment[3].receive_cards(Cards(cards=((1, 1), (2, 1), (3, 1))))
+
+        self.attachment[3].remove_cards(Cards(((1, 1),)))  # test drop cards or card
+
+    def start_gui(self):
+        global CARDS
+        CARDS = tuple([(point, color) for point in range(1, 14) for color in range(4)] + [(14, None), (15, None)])
+        global CARD_IMG
+        CARD_IMG= {"{}_{}".format(point, color): PhotoImage(file="imgs/cards/{}_{}.png".format(point, color)) for
+                    point, color in list(CARDS) + [(None, None)]}
+        self.display()
+        self.appearance.mainloop()
+
+
+
+if __name__ == "__main__":
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+    LOCK = Lock()
+    CLIENT = Client()
+    CLIENT.player_info = {
+        "yangbc": {"points": 1000},
+        "yuanye": {"points": 1000},
+        "lvyaqiao": {"points": 1000},
+    }
+    root = Tk()
+    gamestate = GameState("yangbc", CLIENT.player_info, current_player="yangbc")
+    gamestate.start_game(display=True, master=root)
